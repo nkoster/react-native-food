371f180c4a831aa9176a56cf61746a33fbacc61d
Niels Koster <n.koster@portavita.eu>
2020-04-30 22:00:54 +0200

    SearchBar component starter

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import SearchBar from '../components/SearchBar'
 
 const SearchScreen = _ => {
     return (
         <View>
-            <Text>Search Screen</Text>
+            <SearchBar />
         </View>
     )
 }
