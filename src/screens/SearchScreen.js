02cb053f1d5ffc9988453d58e9276f239bff96fd
Niels Koster <n.koster@portavita.eu>
2020-05-01 23:51:14 +0200

    useEffect() to run search one time at start

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
@@ -7,12 +7,12 @@ const SearchScreen = _ => {
     const [term, setTerm] = useState('')
     const [results, setResults] = useState([])
     const [errorMsg, setErrorMsg] = useState('')
-    const searchApi = async _ => {
+    const searchApi = async searchTerm => {
         try {
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
-                    term,
+                    term: searchTerm,
                     location: 'amsterdam'
                 }
             })
@@ -22,12 +22,15 @@ const SearchScreen = _ => {
             setErrorMsg('Something went wrong')
         }
     }
+    useEffect(_ => {
+        searchApi('pasta')
+    }, [])
     return (
         <View>
             <SearchBar
                 term={term}
                 setTerm={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={_ => searchApi(term)}
             />
             <Text>{errorMsg === '' ? `${results.length} found` : errorMsg}</Text>
         </View>
