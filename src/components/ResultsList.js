a116a84d6c0365591ae576b3060d203f049715b2
Niels Koster <n.koster@portavita.eu>
2020-05-04 01:18:40 +0200

    Navigate to DetailsScreen via received naviation prop

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import ResultsDetail from './ResultsDetail'
 
-const ResultsList = ({title, results}) => {
+const ResultsList = ({title, results, navigation}) => {
     return (
         <View style={styles.container}>
             <Text style={styles.title} >{title}</Text>
@@ -12,7 +12,11 @@ const ResultsList = ({title, results}) => {
                 data={results}
                 keyExtractor={result => result.id}
                 renderItem={({item}) => {
-                return <ResultsDetail detail={item} />
+                return (
+                    <TouchableOpacity onPress={_ => navigation.navigate('DetailsScreen')}>
+                        <ResultsDetail detail={item} />
+                    </TouchableOpacity>
+                )
                 }}
             />
         </View>
