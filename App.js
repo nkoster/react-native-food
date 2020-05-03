ba76718e136a1cfe6581b61ebb0e9e255946eaa4
Niels Koster <n.koster@portavita.eu>
2020-05-04 01:17:43 +0200

    Add DetailsScreen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import SearchScreen from './src/screens/SearchScreen'
+import DetailsScreen from './src/screens/DetailsScreen'
 
 const navigator = createStackNavigator({
-  SearchScreen
+  SearchScreen,
+  DetailsScreen
 }, {
   initialRouteName: 'SearchScreen',
   defaultNavigationOptions: {
