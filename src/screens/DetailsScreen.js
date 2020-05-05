e4a35f921f63dc7ba6f9a8c659ec0fad09e8cdda
Niels Koster <n.koster@portavita.eu>
2020-05-05 14:37:14 +0200

    load detail data from yelp

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, View } from 'react-native'
+import yelp from '../api/yelp'
 
 const DetailsScreen = ({navigation}) => {
+    const [ result, getResult ] = useState(null)
     const id = navigation.getParam('id')
-    console.log('id', id)
+    console.log(result)
+    const getDetails = async id => {
+        const response = await yelp.get(`/${id}`)
+        getResult(response.data)
+    }
+    useEffect(_ => { getDetails(id) }, []) // to run only once
     return (
         <View>
             <Text>Details Screen</Text>
